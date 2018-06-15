68d96c81a704f5f9bb7b55f35eece7509995acd1
Kevin <kevin.noseworthy@stjoseph.com>
2018-06-15 09:35:19 -0400

    sjcArchiveDeployAlphaOld

--- index.php
+++ index.php
@@ -1,3 +1,39 @@
+<?php
+/**
+ * API.PHP
+ *
+ * For Any Archive Request Start Here
+ *
+ * PHP version 7
+ *
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   Application
+ * @package    Request
+ * @author     Kevin Noseworthy <kevin.noseworthy@stjoseph.com>
+ * @copyright  1997-2018 St.Joseph Communication
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version    SVN: Id$
+ * @link       http://pear.php.net/package/PackageName
+ * @see        NetOther, Net_Sample::Net_Sample()
+ * @since      File available since Release 1.2.0
+ * @deprecated File deprecated in Release 2.0.0
+ */ 
+require_once "archive/archive_config_inc.php";
+
+if(isset($_POST['username']) && isset($_POST['passwd']) ) {
+    if(login($_POST['username'], $POST['passwd']) ) {
+        header("Location: dowork.php");
+    }
+}
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s', filemtime(__FILE__)) . ' GMT');
+header("Cache-Control: public");
+header("Pragma: public");
+?>
 <!DOCTYPE HTML>
 <html>
 <head>
