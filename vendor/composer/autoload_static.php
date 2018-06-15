8d6b0cebfc119257fd0c3672ab7af6f671e56fd2
Kevin <kevin.noseworthy@stjoseph.com>
2018-06-15 11:46:49 -0400

    sjcArchiveDeployAlphaOld

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,14 +6,6 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit80245254be2318de5ef3eafeb54a1200
 {
-    public static $files = array (
-        'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
-        'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
-        '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
-        'b067bc7112e384b61c701452d53a14a8' => __DIR__ . '/..' . '/mtdowling/jmespath.php/src/JmesPath.php',
-        '8a9dc1de0ca7e01f3e08231539562f61' => __DIR__ . '/..' . '/aws/aws-sdk-php/src/functions.php',
-    );
-
     public static $prefixLengthsPsr4 = array (
         'R' => 
         array (
@@ -22,7 +14,6 @@ class ComposerStaticInit80245254be2318de5ef3eafeb54a1200
         'P' => 
         array (
             'Psr\\Log\\' => 8,
-            'Psr\\Http\\Message\\' => 17,
         ),
         'M' => 
         array (
@@ -31,20 +22,6 @@ class ComposerStaticInit80245254be2318de5ef3eafeb54a1200
         'L' => 
         array (
             'Lcobucci\\JWT\\' => 13,
-        ),
-        'J' => 
-        array (
-            'JmesPath\\' => 9,
-        ),
-        'G' => 
-        array (
-            'GuzzleHttp\\Psr7\\' => 16,
-            'GuzzleHttp\\Promise\\' => 19,
-            'GuzzleHttp\\' => 11,
-        ),
-        'A' => 
-        array (
-            'Aws\\' => 4,
         ),
     );
 
@@ -57,10 +34,6 @@ class ComposerStaticInit80245254be2318de5ef3eafeb54a1200
         array (
             0 => __DIR__ . '/..' . '/psr/log/Psr/Log',
         ),
-        'Psr\\Http\\Message\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-        ),
         'Monolog\\' => 
         array (
             0 => __DIR__ . '/..' . '/monolog/monolog/src/Monolog',
@@ -68,26 +41,6 @@ class ComposerStaticInit80245254be2318de5ef3eafeb54a1200
         'Lcobucci\\JWT\\' => 
         array (
             0 => __DIR__ . '/..' . '/lcobucci/jwt/src',
-        ),
-        'JmesPath\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/mtdowling/jmespath.php/src',
-        ),
-        'GuzzleHttp\\Psr7\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/guzzlehttp/psr7/src',
-        ),
-        'GuzzleHttp\\Promise\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/guzzlehttp/promises/src',
-        ),
-        'GuzzleHttp\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/guzzlehttp/guzzle/src',
-        ),
-        'Aws\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/aws/aws-sdk-php/src',
         ),
     );
 
