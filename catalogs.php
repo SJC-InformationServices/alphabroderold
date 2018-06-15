eb0a3cf29d9d4e9560bfb8ae6a94c5dfecead61b
Kevin <kevin.noseworthy@stjoseph.com>
2018-06-15 10:35:30 -0400

    sjcArchiveDeployAlphaOld

---
+++ catalogs.php
@@ -0,0 +1,20 @@
+<?php
+include "archive/archive_config_inc.php";
+include "archive/alpha_catalogs/alpha_catalogs.php";
+
+$path = ltrim($_SERVER['REQUEST_URI'], '/');    // Trim leading slash(es)
+$name = 'alpha_catalogs';
+
+$path = substr($path,strpos($path,$name));
+try{
+	$API = new archive($path, null,$archiveconfig);
+    echo $API->processAPI();
+	
+} 
+catch (Exception $e){
+	  echo json_encode(Array('error' => $e->getMessage()));
+	}
+	
+
+
+?>
---
+++ colors.php
@@ -0,0 +1,20 @@
+<?php
+include "archive/archive_config_inc.php";
+include "archive/alpha_styles/alpha_styles.php";
+
+$path = ltrim($_SERVER['REQUEST_URI'], '/');    // Trim leading slash(es)
+$name = 'alpha_colors';
+
+$path = substr($path,strpos($path,$name));
+try{
+	$API = new archive($path, null,$archiveconfig);
+    echo $API->processAPI();
+	
+} 
+catch (Exception $e){
+	  echo json_encode(Array('error' => $e->getMessage()));
+	}
+	
+
+
+?>
