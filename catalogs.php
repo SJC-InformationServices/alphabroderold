3fe4823eb7f44b61756a2539dae2b268a5953b45
Kevin <kevin.noseworthy@stjoseph.com>
2018-06-15 14:22:09 -0400

    sjcArchiveDeployAlphaOld

--- catalogs.php
+++ catalogs.php
@@ -6,6 +6,8 @@ $path = ltrim($_SERVER['REQUEST_URI'], '/');    // Trim leading slash(es)
 $name = 'alpha_catalogs';
 
 $path = substr($path,strpos($path,$name));
+echo "$path";
+/*
 try{
 	$API = new archive($path, null,$archiveconfig);
     echo $API->processAPI();
@@ -14,7 +16,7 @@ try{
 catch (Exception $e){
 	  echo json_encode(Array('error' => $e->getMessage()));
 	}
-	
+*/	
 
 
 ?>
